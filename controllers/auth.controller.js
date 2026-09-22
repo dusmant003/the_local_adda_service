6aa7ca650e66a647d76fdfcb6781ae748194d43f
dusmant003 <dusmantameher449@gmail.com>
2026-09-22 09:06:55 +0530

    create register and login router

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { executeQuery } = require('../config/db');
 
 const registerUser = async (req, res) => {
@@ -57,6 +58,73 @@ const registerUser = async (req, res) => {
     }
 }
 
+// login user
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // validate required fields
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "all fields are required",
+                success: false
+            })
+        }
+
+
+        // fins user by email
+        const user = await executeQuery(
+            `SELECT * FROM users WHERE email =?`,
+            [email]
+        );
+
+        if (user.length === 0) {
+            return res.status(400).json({
+                message: "user not found",
+                success: false
+            })
+        }
+
+        // compare password
+
+        const isPasswordValid = await bcrypt.compare(password, user[0].password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                message: "invalid password",
+                success: false
+            })
+        }
+        // create jwt token
+        const token = jwt.sign({ id: user[0].id }, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+
+        });
+
+        // return success response
+        return res.status(200).json({
+            message: "user logged in successfully",
+            success: true,
+            user: {
+                id: user[0].id,
+                name: user[0].name,
+                email: user[0].email,
+                phone: user[0].phone
+            },
+            token
+        })
+
+    } catch (error) {
+        console.error("login error:", error.message)
+        return res.status(500).json({
+            message: "internal server error",
+            success: false
+        })
+
+    }
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
 }
